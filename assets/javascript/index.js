5cb9b0e5f218924b04fbf49af27d7f5490ab8254
Jonas Tiago <jonastiago95@gmail.com>
2022-12-27 23:43:14 -0300

    feat: function encrypt

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,23 @@
+const noanswer = document.querySelector(".noanswer");
+const answer = document.querySelector(".answer");
+const text = document.querySelector("#cript");
+const bntCopy = document.querySelector(".copy");
 
+function encrypt() {
+  if (text.value.length > 0) {
+    const textEncrypt = text.value
+      .replace(/e/g, "enter")
+      .replace(/i/g, "imes")
+      .replace(/a/g, "ai")
+      .replace(/o/g, "ober")
+      .replace(/u/g, "ufat");
 
+    noanswer.classList.add("disappear");
 
+    answer.classList.remove("disappear");
+
+    bntCopy.classList.remove("disappear");
+
+    answer.textContent = textEncrypt;
+  }
+}
